c34b75512ecf91d99bf80cae8cedf7ee37fe58d4
LuisaB <lbetancur72@gmail.com>
2024-09-16 21:40:27 -0500

    Se crea nuevos botones

--- components/tokens.ts
+++ components/tokens.ts
@@ -16,6 +16,11 @@ export const standardSizeIcon = 30
 export const logoSizeIcon = 115
 
 
+export const standardTextProfileRight = `text-base text-gray-700 text-right `;
+export const textSubtitleContainer = `flex flex-row items-end justify-between w-80 px-4 mb-4 pl-3`;
+export const textSubtitleEmail = `text-lg text-center pl-3.5 mb-4`;
+
+
 export const modalContainer = `flex justify-end items-center flex-1 `; 
 export const modalContent = `w-80 h-60 bg-white p-6 rounded-lg shadow-lg border border-gray-300`; 
 
@@ -24,12 +29,6 @@ export const avatarContainer = `w-40 h-30 rounded-full justify-center items-cent
 export const avatarButtonContainer = `flex flex-row items-center w-full px-4 mb-8`;
 
 export const avatarImage = `w-24 h-24 rounded-full`;
-
-export const standardTextProfileRight = `text-base text-gray-700 text-right `;
-
-export const textSubtitleContainer = `flex flex-row items-end justify-between w-80 px-4 mb-4 pl-3`;
-
-export const textSubtitleEmail = `text-lg text-center pl-3.5 mb-4`;
 
 export const buttonContainer = 'flex items-center my-1';
 
