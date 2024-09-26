9283c56925c3efb0bee06d5718e8aaa6b877a2c8
Girveos <jero713@hotmail.com>
2024-09-26 17:58:50 -0500

    Corrección de login con validación de ñ opción para ver la contraseña ingresada no vaciar los textos de los campos cuando aparezca el modal limitar el scrollview para que no se salga de la interfaz bottom bar en ticket request

--- components/screens/Tickets.tsx
+++ components/screens/Tickets.tsx
@@ -2,9 +2,12 @@ import { View, Text, ScrollView } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { AddButton } from "../atoms/CustomButton";
 import { useRouter } from "expo-router";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { TitleTextTickets } from "../atoms/TitleText";
-import BottomBar from "../organisms/BottomBar"
+import BottomBar from "../organisms/BottomBar";
+
+import * as Tokens from "../tokens";
+import { getTickets } from "../../config/routers";
 
 export default function Tickets() {
   const insets = useSafeAreaInsets();
@@ -17,38 +20,81 @@ export default function Tickets() {
     router.push("/ticketrequest");
   };
 
+  useEffect(() => {
+    const fetchTickets = async () => {
+      const response = await getTickets();
+      if (response.success) {
+        setTickets(response.data);
+      } else {
+        console.error(response.message);
+      }
+    };
+
+    fetchTickets();
+  }, []);
+
   return (
-    <View className="flex-1 w-full mt-9 justify-between " style={{ paddingBottom: insets.bottom }}>
+    <View
+      className="flex-1 w-full mt-9 justify-between"
+      style={{ paddingBottom: insets.bottom }}
+    >
       <View className="justify-center items-center border-b border-slate-200">
-        <TitleTextTickets/>
+        <TitleTextTickets />
       </View>
-      <View className="w-full h-full flex justify-start items-center">
-      <ScrollView className="w-full flex-1">
-        {tickets.length > 0 ? (
-          tickets.map((ticket) => (
-            <View key={ticket /* .id */} className="w-11/12 h-24 m-5 flex flex-row justify-around items-center rounded-lg bg-gray-200">
-              <View>
-                <Text className="font-bold text-lg">Tipo de solicitud: {/* ticket.tipo */}</Text>
-                <Text className="text-lg">Título: {/* ticket.titulo */}</Text>
-                <Text className="text-lg">Fecha: {/* {ticket.fecha */}</Text>
+      <View className="w-full flex-1 justify-centerb  items-center">
+        <ScrollView
+          contentContainerStyle={{ flexGrow: 1 }}
+          showsVerticalScrollIndicator={false}
+        >
+          {tickets.length > 0 ? (
+            tickets.map((ticketMap) => (
+              <View
+                key={ticketMap}
+                className="mt-5 w-full h-24 flex-row justify-center items-center rounded-lg bg-gray-200"
+              >
+                <View>
+                  <Text className="font-bold text-lg">
+                    Tipo de solicitud:{" "}
+                    <Text className="text-md font-normal">
+                      {ticketMap.type}
+                    </Text>
+                  </Text>
+                  <Text className="font-bold text-lg">
+                    Título:{" "}
+                    <Text className="text-md font-normal">
+                      {ticketMap.title}
+                    </Text>
+                  </Text>
+                  <Text className=" font-bold text-lg">
+                    Fecha:{" "}
+                    <Text className="font-normal text-md">
+                      {new Date(ticketMap.start_date).toLocaleDateString(
+                        "es-ES",
+                        {
+                          year: "numeric",
+                          month: "long",
+                          day: "numeric",
+                        }
+                      )}
+                    </Text>
+                  </Text>
+                </View>
+                <View className="h-full flex flex-col items-end left-5 mt-5">
+                  <View className="w-[15] h-[15] rounded-full bg-orange-500"></View>
+                </View>
               </View>
-              <View className="h-full flex flex-col items-end justify-around">
-                <View className="w-[30] h-[30] rounded-full bg-orange-500"></View>
-                <View><Text>Ticket #{/* ticket.id */}</Text></View>
-              </View>
-            </View>
-          ))
-        ) : (
-          <Text className="text-lg mt-5">No tienes solicitudes aún.</Text>
-        )}
-      </ScrollView>
+            ))
+          ) : (
+            <Text className={`${Tokens.standardSubtitleLogin} mt-5`}>
+              No tienes solicitudes aún
+            </Text>
+          )}
+        </ScrollView>
+        <View className="absolute bottom-16 right-6">
+          <AddButton text="+" customFun={handlePress} />
+        </View>
+        <BottomBar activeRoute="/tickets" />
       </View>
-      <View className="absolute bottom-16 right-6">
-        <AddButton text="+" customFun={handlePress} />
-      </View>
-      <BottomBar activeRoute="/tickets"/>
     </View>
   );
 }
-
-
--- config/routers.ts
+++ config/routers.ts
@@ -1,41 +1,64 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const ip1 = "http://10.0.2.2";
+const ip2 = "http://192.168.0.149";
 
 export const loginRequest = async (user: string, pass: string) => {
-    try {
-      const response = await fetch('http://192.168.1.75:3001/api/v1/login', {
-        /* para emulador la ip es 10.0.2.2 */
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email: user, password: pass }),
-      });
-  
-      const data = await response.json();
-      if (response.ok) {
-        await AsyncStorage.setItem('token', data.access);
-        return { success: true, data };     
-      } else {
-        switch (response.status) {
-          case 400:
-            return { success: false, message: "Solicitud incorrecta. Revisa los datos ingresados." };
-          case 401:
-            return { success: false, message: "Correo o contraseña incorrectos." };
-          case 403:
-            return { success: false, message: "Acceso denegado. Verifica tus credenciales." };
-          case 404:
-            return { success: false, message: "El servicio no está disponible. Intenta más tarde." };
-          case 500:
-            return { success: false, message: "Error en el servidor. Intenta más tarde." };
-          default:
-            return { success: false, message: "Error desconocido. Intenta más tarde." };
-        }
+  try {
+    const response = await fetch(`${ip1}:3001/api/v1/login`, {
+      /* 192.168.1.75 */
+      /* para emulador la ip es 10.0.2.2 */
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email: user, password: pass }),
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      await AsyncStorage.setItem("token", data.access);
+      return { success: true, data };
+    } else {
+      switch (response.status) {
+        case 400:
+          return {
+            success: false,
+            message: "No fue posible la conexión con la base de datos.",
+          };
+        case 401:
+          return {
+            success: false,
+            message: "Correo o contraseña incorrectos.",
+          };
+        case 403:
+          return {
+            success: false,
+            message: "Acceso denegado. Verifica tus credenciales.",
+          };
+        case 404:
+          return {
+            success: false,
+            message: "El servicio no está disponible. Intenta más tarde.",
+          };
+        case 500:
+          return {
+            success: false,
+            message: "Error en el servidor. Intenta más tarde.",
+          };
+        default:
+          return {
+            success: false,
+            message: "Error desconocido. Intenta más tarde.",
+          };
       }
-    } catch (error) {
-      console.error('Error al iniciar sesión:', error);
     }
-  };
-  
+  } catch (error) {
+    console.error("Error de red. Verifica tu conexión.");
+  }
+};
+
 export const createTicket = async (
   start_date: string,
   end_date: string,
@@ -45,7 +68,7 @@ export const createTicket = async (
   user_id: number
 ) => {
   try {
-    const response = await fetch("xxxxxxxxx", {
+    const response = await fetch(`${ip1}:3001/api/v1/XXXX`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -58,7 +81,7 @@ export const createTicket = async (
         description: description,
         state: "pendiente",
         user_id: user_id,
-      })
+      }),
     });
 
     const data = await response.json();
@@ -97,7 +120,67 @@ export const createTicket = async (
   } catch (error) {
     return {
       success: false,
-      message: "Error de red o servidor. Verifica tu conexión.",
+      message: "Error de red. Verifica tu conexión.",
     };
+  }
+};
+
+export const getTickets = async () => {
+  try {
+    const token = await AsyncStorage.getItem("token");
+    const response = await fetch(`${ip1}:3001/api/v1/request/me`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      return { success: true, data };
+    }else {
+      switch (response.status) {
+        case 404:
+          return {
+            success: true,
+            data:0,
+          };
+      }
+    }
+  } catch (error) {
+    console.error("Error al obtener tickets:", error);
+    return { success: false, message: "Error de red. Verifica tu conexión." };
+  }
+};
+
+export const getUserInfo = async () => {
+  try {
+    const token = await AsyncStorage.getItem("token");
+    const response = await fetch(`${ip1}:3001/api/v1/user/me`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      return { success: true, data };
+    }else {
+      switch (response.status) {
+        case 404:
+          return {
+            success: true,
+            data:0,
+          };
+      }
+    }
+  } catch (error) {
+    console.error("Error al obtener el usuario:", error);
+    return { success: false, message: "Error de red. Verifica tu conexión." };
   }
 };
