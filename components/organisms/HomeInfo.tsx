c788c1e909f9c80440334c2eb8d56d269445fe9f
ValentinaMarinG <valentinamaringalvis28@gmail.com>
2024-10-02 18:27:28 -0500

    Se agrego la información del usuario en sesión en la screen del home

--- components/organisms/HomeInfo.tsx
+++ components/organisms/HomeInfo.tsx
@@ -1,16 +1,44 @@
-import { View } from "react-native";
+import { View, Text } from "react-native";
 import { ArrowIcon } from "../atoms/Icon";
 import * as Tokens from "../tokens";
 import  { ProfilePhotoHome } from "../atoms/ProfilePhoto";
 import {CompanyHomeCard, NameHomeCard, ProfileHomeCard} from "../atoms/DescriptionText";
 import { Link} from "expo-router";
+import { useEffect, useState } from 'react';
+import { getUserInfo } from '../../config/routers';
+
 export default function HomeCard() {
+
+  const [account, SetAccount] = useState({
+    name: "",
+    lastname:""
+  });
+
+  useEffect(() => {
+    SetAccount({
+      name: "",
+      lastname:""
+    });
+    const fetchUser = async () => {
+      const response = await getUserInfo();
+      if (response?.success) {
+        SetAccount({
+          name: response?.data.name,
+          lastname: response?.data.lastname
+        });
+      } else {
+        console.error(response?.message);
+      }
+    };
+    fetchUser();
+  }, []);
+
   return (
     <View className="bg-cardColor w-full rounded-2xl p-5 ">
         <View className="flex-row items-center">
         <ProfilePhotoHome/>
             <View className="flex-1 justify-between ml-5">
-            <NameHomeCard/>
+            <Text className="text-lg text-cardColorText text-left">{account.name} {account.lastname}</Text>
             <ProfileHomeCard/>
             </View>
         <Link href={"/profile"}>
--- components/screens/Home.tsx
+++ components/screens/Home.tsx
@@ -1,4 +1,4 @@
-import { View, ScrollView } from 'react-native';
+import { View, ScrollView, Text } from 'react-native';
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import BottomBar from "../organisms/BottomBar";
 import { ShiftTextHome, SubTitleTextHome } from '../atoms/SubtitleText';
@@ -6,15 +6,43 @@ import { TitleTextHome } from '../atoms/TitleText';
 import HomeCard from '../organisms/HomeInfo';
 import { SearchInput} from '../organisms/SearchInput';
 import ShiftsList from '../organisms/ShiftsList';
+import { useEffect, useState } from 'react';
+import { getUserInfo } from '../../config/routers';
+import * as Tokens from "../tokens";
 
 export default function Home() {
+  const [account, SetAccount] = useState({
+    name: "",
+    lastname:""
+  });
+
+
+  useEffect(() => {
+    SetAccount({
+      name: "",
+      lastname:""
+    });
+    const fetchUser = async () => {
+      const response = await getUserInfo();
+      if (response?.success) {
+        SetAccount({
+          name: response?.data.name,
+          lastname: response?.data.lastname
+        });
+      } else {
+        console.error(response?.message);
+      }
+    };
+    fetchUser();
+  }, []);
+
   return (
     <View className="flex-1 w-full justify-between">
       <View className="flex-1 justify-between px-5 mt-12">
         <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false}>
           <View className="mb-5">
             <SubTitleTextHome />
-            <TitleTextHome />
+            <Text className={`${Tokens.standardTextTitleBold}`}>Hola {account.name}</Text>
           </View>
           <View className="mb-5">
             <HomeCard />
