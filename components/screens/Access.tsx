bfe8a89f7d6cf506a43a7741bcd9e4e6c4c8237c
ValentinaMarinG <valentinamaringalvis28@gmail.com>
2024-10-08 14:20:15 -0500

    Se cambiaron las validaciones de los formularios de solicitar acceso (access)  y crear ticket (ticketRequest)  a validaciones implementando react form hook

--- components/screens/Access.tsx
+++ components/screens/Access.tsx
@@ -8,61 +8,45 @@ import {
   TouchableWithoutFeedback,
   Keyboard,
   ScrollView,
-  Text
+  Text,
 } from "react-native";
 import { useState } from "react";
+import { useRouter } from "expo-router";
+import { useForm, Controller } from "react-hook-form";
+
 import { TitleTextAccess } from "../atoms/TitleText";
 import { SubTitleTextAccess } from "../atoms/SubtitleText";
 import { AccessModal, LoginUserText } from "../atoms/DescriptionText";
 import { CustomButton } from "../atoms/CustomButton";
-import { useRouter } from "expo-router";
-import { SafeAreaView } from "react-native-safe-area-context";
 import { MainIcon } from "../atoms/Icon";
-
 import { accessRequest } from "../../config/routers";
-
 import * as Tokens from "../tokens";
 
-export default function Login() {
-  const [modalVisible, setModalVisible] = useState(false);
-  const [email, setEmail] = useState("");
+type FormData = {
+  email: string;
+};
 
-  const  [emailError, setEmailError] = useState("")
-
+export default function Access() {
   const router = useRouter();
 
-  const handlePress = async () => {
-    if (validate()) {
-      const response = await accessRequest(email);
-      
-      if (response.success) {
-        setModalVisible(true);
-      } else {
-        Alert.alert("Error", response.message); 
-      }
+  const { control, handleSubmit, formState: { errors } } = useForm<FormData>();
+
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const onSubmit = handleSubmit(async (data) => {
+    const response = await accessRequest(data.email);
+  
+    if (response.success) {
+      setModalVisible(true);
+    } else {
+      Alert.alert("Error", response.message);
     }
-  };
+  });
 
   const handleModalClose = () => {
     setModalVisible(false);
     router.push("/login");
   };
-
-  const validate = () => {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  
-    if (!email) {
-      setEmailError("El correo es obligatorio.");
-      return false;
-    } else if (!emailRegex.test(email)) {
-      setEmailError("El correo ingresado no es válido.");
-      return false;
-    } else {
-      setEmailError("");
-      return true;
-    }
-  };
-
 
   return (
     <KeyboardAvoidingView
@@ -85,21 +69,34 @@ export default function Login() {
                 <SubTitleTextAccess />
               </View>
             </View>
-            <View className="w-3/4 m-5">
+            <View className="w-3/4 mt-5">
               <LoginUserText />
-              <TextInput
-                className={`${Tokens.standardInput}`}
-                onChangeText={(text) => {
-                  setEmail(text);
-                  if (emailError) {
-                    setEmailError(""); 
+              <Controller
+                control={control}
+                rules={{
+                  required: "El correo es obligatorio.",
+                  pattern: {
+                    value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                    message: "El correo ingresado no es válido."
                   }
                 }}
-                value={email}
+                render={({ field: { onChange, onBlur, value } }) => (
+                  <TextInput
+                    className={`${Tokens.standardInput}`}
+                    onBlur={onBlur}
+                    onChangeText={onChange}
+                    value={value}
+                    keyboardType="email-address"
+                  />
+                )}
+                name="email"
               />
-              {emailError ? <Text className="text-red-500 mt-2">{emailError}</Text> : null}
-              <View className="my-4 items-center justify-center">
-                <CustomButton text="Realizar solicitud" customFun={handlePress} />
+              {errors.email && <Text className="text-red-500 mt-2">{errors.email.message}</Text>}
+              <View className="my-5 items-center justify-center">
+                <CustomButton
+                  text="Realizar solicitud"
+                  customFun={onSubmit}
+                />
               </View>
             </View>
           </View>
@@ -113,12 +110,12 @@ export default function Login() {
             }}
           >
             <View className="flex-1 justify-center items-center bg-[#858585] opacity-90">
-            <View className="flex-1 justify-center items-center bg-black">
-              <View className="absolute bg-white p-6 rounded-lg w-3/4 items-center">
-                <AccessModal />
-                <CustomButton text="Salir" customFun={handleModalClose} />
+              <View className="flex-1 justify-center items-center bg-black">
+                <View className="absolute bg-white p-6 rounded-lg w-3/4 items-center">
+                  <AccessModal />
+                  <CustomButton text="Salir" customFun={handleModalClose} />
+                </View>
               </View>
-            </View>
             </View>
           </Modal>
         </ScrollView>
@@ -126,5 +123,3 @@ export default function Login() {
     </KeyboardAvoidingView>
   );
 }
-
-
